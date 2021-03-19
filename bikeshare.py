4ebc6ff4eeca99e24583a904b1f0abb19b483112
Daad <daad.f.o.5@gmail.com>
2021-03-19 23:06:31 +0300

    docs: Retype of def main

--- bikeshare.py
+++ bikeshare.py
@@ -193,19 +193,17 @@ def user_stats(df):
 
 
 def main():
+
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
-
 
 if __name__ == "__main__":
 	main()
